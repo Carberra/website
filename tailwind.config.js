6af7088b389645d3c3416a8d1ae7ab3bcec2851e
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-06-04 16:10:41 -0400

    Add basic tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: ['./src/**/*.tsx', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Helvetica',
+        'Arial',
+        'sans-serif',
+      ],
+    },
+  },
+  plugins: [],
+};
