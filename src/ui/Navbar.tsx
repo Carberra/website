cd79a4431a37f19a5f4ec9a944a841b38e37e41d
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-06-05 17:00:06 -0400

    Make navbar sticky

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -4,7 +4,7 @@ interface Props {}
 
 export const Navbar: React.FC<Props> = () => {
   return (
-    <nav className="h-14 bg-black flex justify-center items-center border-solid border-b-2 border-white">
+    <nav className="h-14 bg-black flex justify-center items-center border-solid border-b-2 border-white sticky w-full">
       <p className="text-white font-brand text-xl">Navbar</p>
     </nav>
   );
