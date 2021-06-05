6185a640471ae16408432c4a76d274030bb8f22b
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-06-04 22:18:02 -0400

    Include navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { Navbar } from '@ui/Navbar';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
         <meta name="description" content="The home of Carberra Tutorials" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar />
     </div>
   );
 }
