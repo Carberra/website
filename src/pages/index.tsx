266d9ced2fcaf319a228198550d23698f7c9cccc
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-06-05 17:15:36 -0400

    Add logo to the center of the screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import Head from 'next/head';
+
 import { Navbar } from '@ui/Navbar';
+
+import LogoWhite from '@public/brand/logo-white.svg';
 
 export default function Home() {
   return (
@@ -10,6 +13,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
+      <div className="bg-black h-screen-navbar">
+        <div className="absolute w-full h-full top-0 left-0 table">
+          <div className="table-cell align-middle">
+            <div className="w-min ml-auto mr-auto">
+              <LogoWhite className="w-44" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
