062f9ac0b409c4730b1ddcb53adef9f60a7a6709
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-06-05 20:30:10 -0400

    Impliment favicon on all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,44 @@
 import '@styles/globals.scss';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>Carberra Tutorials</title>
+        <meta name="description" content="The home of Carberra Tutorials" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#0c0c0c"
+        />
+        <meta name="apple-mobile-web-app-title" content="Carberra Tutorials" />
+        <meta name="application-name" content="Carberra Tutorials" />
+        <meta name="msapplication-TileColor" content="#0c0c0c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 export default MyApp;
