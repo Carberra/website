4b0a782bf02a96b79566c69c38d5820a99a543d5
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-06-11 17:36:05 -0400

    Export props and change to type instead of interface

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-interface Props {}
+export type NavbarProps = {};
 
-export const Navbar: React.FC<Props> = () => {
+export const Navbar: React.FC<NavbarProps> = () => {
   return (
     <nav className="h-14 bg-black flex justify-center items-center border-solid border-b-2 border-white sticky w-full">
       <p className="text-white font-brand text-xl">Navbar</p>
