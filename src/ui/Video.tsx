2b448ae4a9b90645d984c7ed1b2527c4d28ef505
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-06-16 09:30:24 -0400

    Add base component for video

---
+++ src/ui/Video.tsx
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+type Props = {};
+type States = {};
+
+export default class Video extends Component<Props, States> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
