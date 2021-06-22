42ae4fe9fb078154ce2ac984dcd8db8e6dda3ec9
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-06-22 13:32:03 -0400

    Replace next images with standard img tags

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,14 @@
 import '@styles/globals.scss';
 
+import * as nextImage from 'next/image';
+
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: (props) => {
+    return <img {...props} />;
+  },
+});
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
