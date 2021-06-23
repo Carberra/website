a85849f75d8f2d0718ffd349a4473a58d6cb97a1
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-06-23 14:05:22 -0400

    Move platforms section to a different render function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,7 +49,7 @@ export default class Home extends Component<Props, States> {
     }, 250);
   };
 
-  render() {
+  renderPlatformsSection = () => {
     const iconContainerClasses = 'flex justify-center items-center';
     const iconClasses = 'w-10 xs:w-11';
     const iconContainerMargin = '20';
@@ -62,106 +62,116 @@ export default class Home extends Component<Props, States> {
     const iconSpreadPairSm = '9';
 
     return (
+      <div className="bg-black h-screen-navbar">
+        <div className="absolute w-full h-full top-0 left-0 table">
+          <div className="table-cell align-middle">
+            <div className="w-min ml-auto mr-auto">
+              <div
+                className={`${iconContainerClasses} mb-${iconContainerMarginSm} xs:mb-${iconContainerMargin}`}
+              >
+                <a
+                  href="https://github.carberra.xyz"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={`relative right-${iconSpreadXSm} xs:right-${iconSpreadX} top-${iconSpreadYSm} xs:top-${iconSpreadY}`}
+                >
+                  <GithubIcon className={iconClasses} />
+                </a>
+                <a
+                  href="https://discord.carberra.xyz"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={`relative right-${iconSpreadPairSm} xs:right-${iconSpreadPair}`}
+                >
+                  <DiscordIcon className={iconClasses} />
+                </a>
+                <a
+                  href="https://youtube.carberra.xyz"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={`relative left-${iconSpreadPairSm} xs:left-${iconSpreadPair}`}
+                >
+                  <YoutubeIcon className={iconClasses} />
+                </a>
+                <a
+                  href="https://twitch.carberra.xyz"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={`relative left-${iconSpreadXSm} xs:left-${iconSpreadX} top-${iconSpreadYSm} xs:top-${iconSpreadY}`}
+                >
+                  <TwitchIcon className={iconClasses} />
+                </a>
+              </div>
+              <LogoWhite
+                className="w-32 xs:w-44 mx-auto"
+                onClick={this.handleLogoClick}
+              />
+              <div
+                className={`${iconContainerClasses} mt-${iconContainerMarginSm} xs:mt-${iconContainerMargin}`}
+              >
+                <a
+                  href="https://lbry.carberra.xyz"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={`relative right-${iconSpreadXSm} xs:right-${iconSpreadX} bottom-${iconSpreadYSm} xs:bottom-${iconSpreadY}`}
+                >
+                  <LbryIcon className={iconClasses} />
+                </a>
+                <a
+                  href="https://twitter.carberra.xyz"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={`relative right-${iconSpreadPairSm} xs:right-${iconSpreadPair}`}
+                >
+                  <TwitterIcon className={iconClasses} />
+                </a>
+                <a
+                  href="https://facebook.carberra.xyz"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={`relative left-${iconSpreadPairSm} xs:left-${iconSpreadPair}`}
+                >
+                  <FacebookIcon className={iconClasses} />
+                </a>
+                <a
+                  href="https://patreon.carberra.xyz"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={`relative left-${iconSpreadXSm} xs:left-${iconSpreadX} bottom-${iconSpreadYSm} xs:bottom-${iconSpreadY}`}
+                >
+                  <PatreonIcon className={iconClasses} />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="absolute w-full bottom-12 xs:bottom-14">
+          <h2
+            className={
+              'text-white font-brand lowercase text-center text-xl px-8 transition duration-400 ease-in-out ' +
+              this.state.scrollTextFade
+            }
+          >
+            {this.state.scrollDownText}
+          </h2>
+        </div>
+      </div>
+    );
+  };
+
+  renderVideosSection = () => {
+    return <div></div>;
+  };
+
+  render() {
+    return (
       <div>
         <Head>
           <title>Carberra Tutorials</title>
           <meta name="description" content="The home of Carberra Tutorials" />
         </Head>
         <Navbar />
-        <div className="bg-black h-screen-navbar">
-          <div className="absolute w-full h-full top-0 left-0 table">
-            <div className="table-cell align-middle">
-              <div className="w-min ml-auto mr-auto">
-                <div
-                  className={`${iconContainerClasses} mb-${iconContainerMarginSm} xs:mb-${iconContainerMargin}`}
-                >
-                  <a
-                    href="https://github.carberra.xyz"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={`relative right-${iconSpreadXSm} xs:right-${iconSpreadX} top-${iconSpreadYSm} xs:top-${iconSpreadY}`}
-                  >
-                    <GithubIcon className={iconClasses} />
-                  </a>
-                  <a
-                    href="https://discord.carberra.xyz"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={`relative right-${iconSpreadPairSm} xs:right-${iconSpreadPair}`}
-                  >
-                    <DiscordIcon className={iconClasses} />
-                  </a>
-                  <a
-                    href="https://youtube.carberra.xyz"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={`relative left-${iconSpreadPairSm} xs:left-${iconSpreadPair}`}
-                  >
-                    <YoutubeIcon className={iconClasses} />
-                  </a>
-                  <a
-                    href="https://twitch.carberra.xyz"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={`relative left-${iconSpreadXSm} xs:left-${iconSpreadX} top-${iconSpreadYSm} xs:top-${iconSpreadY}`}
-                  >
-                    <TwitchIcon className={iconClasses} />
-                  </a>
-                </div>
-                <LogoWhite
-                  className="w-32 xs:w-44 mx-auto"
-                  onClick={this.handleLogoClick}
-                />
-                <div
-                  className={`${iconContainerClasses} mt-${iconContainerMarginSm} xs:mt-${iconContainerMargin}`}
-                >
-                  <a
-                    href="https://lbry.carberra.xyz"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={`relative right-${iconSpreadXSm} xs:right-${iconSpreadX} bottom-${iconSpreadYSm} xs:bottom-${iconSpreadY}`}
-                  >
-                    <LbryIcon className={iconClasses} />
-                  </a>
-                  <a
-                    href="https://twitter.carberra.xyz"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={`relative right-${iconSpreadPairSm} xs:right-${iconSpreadPair}`}
-                  >
-                    <TwitterIcon className={iconClasses} />
-                  </a>
-                  <a
-                    href="https://facebook.carberra.xyz"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={`relative left-${iconSpreadPairSm} xs:left-${iconSpreadPair}`}
-                  >
-                    <FacebookIcon className={iconClasses} />
-                  </a>
-                  <a
-                    href="https://patreon.carberra.xyz"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={`relative left-${iconSpreadXSm} xs:left-${iconSpreadX} bottom-${iconSpreadYSm} xs:bottom-${iconSpreadY}`}
-                  >
-                    <PatreonIcon className={iconClasses} />
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="absolute w-full bottom-12 xs:bottom-14">
-            <h2
-              className={
-                'text-white font-brand lowercase text-center text-xl px-8 transition duration-400 ease-in-out ' +
-                this.state.scrollTextFade
-              }
-            >
-              {this.state.scrollDownText}
-            </h2>
-          </div>
-        </div>
+        {this.renderPlatformsSection()}
       </div>
     );
   }
