9b5093cde0bdf198ff61e7e902a88a08b7a70919
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-06-24 12:52:47 -0400

    Add handling for left or right side

--- src/ui/Video.tsx
+++ src/ui/Video.tsx
@@ -1,7 +1,14 @@
 import { Component } from 'react';
 import Image from 'next/image';
 
-export type VideoProps = {};
+const videoSide: { [key: string]: string } = {
+  left: 'justify-start',
+  right: 'justify-end',
+};
+
+export type VideoProps = {
+  side?: keyof typeof videoSide;
+};
 type VideoStates = {
   videoTitle: string;
   videoThumbnail: string;
@@ -9,6 +16,10 @@ type VideoStates = {
 };
 
 export class Video extends Component<VideoProps, VideoStates> {
+  static defaultProps = {
+    side: videoSide.left,
+  };
+
   constructor(props: VideoProps) {
     super(props);
     this.state = {
@@ -19,27 +30,45 @@ export class Video extends Component<VideoProps, VideoStates> {
     };
   }
 
+  renderVideoInfo = () => {
+    return <h1 className="text-2xl text-white font-brand">Video info</h1>;
+  };
+
   render() {
     return (
       <div className="flex">
-        <div className="inline-flex">
-          <a
-            href={this.state.videoUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src={this.state.videoThumbnail}
-              alt={this.state.videoTitle}
-              width={320}
-              height={180}
-              className="cursor-pointer"
-            />
-          </a>
-          <div className="flex justify-center items-center">
-            <div className="absolute w-full left-0 flex justify-center">
-              <div className="w-16 border-b-4 border-white"></div>
-            </div>
+        {Object.keys(videoSide).find(
+          (key: string) => videoSide[key] === this.props.side
+        ) === 'right' ? (
+          <div className="w-full">{this.renderVideoInfo()}</div>
+        ) : null}
+        <div className={`flex ${this.props.side} w-full`}>
+          <div className="inline-flex">
+            <a
+              href={this.state.videoUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src={this.state.videoThumbnail}
+                alt={this.state.videoTitle}
+                width={320}
+                height={180}
+                className="cursor-pointer"
+              />
+            </a>
+          </div>
+        </div>
+        {Object.keys(videoSide).find(
+          (key: string) => videoSide[key] === this.props.side
+        ) === 'left' ? (
+          <div className="w-full flex justify-end">
+            {this.renderVideoInfo()}
+          </div>
+        ) : null}
+        <div className="flex justify-center items-center">
+          <div className="absolute w-full left-0 flex justify-center">
+            <div className="w-16 border-b-4 border-white"></div>
           </div>
         </div>
       </div>
