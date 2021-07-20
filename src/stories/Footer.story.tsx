417d4f2c0c4f2533607b1320f7bbd8db9c66a1d8
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-07-20 19:13:27 -0400

    Use viewport for mobile view

--- src/stories/Footer.story.tsx
+++ src/stories/Footer.story.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
 import { Story } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { Footer, FooterProps } from '@ui/Footer';
 
 export default {
   title: 'Footer',
   component: Footer,
+  parameters: {
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+      defaultViewport: 'reponsive',
+    },
+  },
 };
 
 export const Main: Story<FooterProps> = () => <Footer />;
 
-export const Mobile: Story<FooterProps> = () => (
-  <div style={{ width: '508px' }}>
-    <Footer />
-  </div>
-);
+export const Mobile: Story<FooterProps> = () => <Footer />;
+
+Mobile.parameters = {
+  viewport: {
+    defaultViewport: 'iphonex',
+  },
+};
