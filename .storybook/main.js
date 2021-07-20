9f971654e1bb563727095701b669b736df7037d0
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-07-20 16:37:00 -0400

    Use webpack 5 and use svgr for loading svgs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,9 @@
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
   stories: ['../src/stories/**/*.story.@(ts|tsx|js|jsx|mdx)'],
   addons: [
     '@storybook/addon-links',
@@ -14,11 +17,27 @@ module.exports = {
       },
     },
   ],
-  webpackFinal: async (config) => {
+  webpackFinal: (config) => {
+    // SVGR Loader
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test('.svg')
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    // SASS/PostCSS Loader
+
     config.module.rules.push({
       test: /\.scss$/,
       use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
     });
+
+    // TS Paths
+
     config.resolve.plugins = [new TsconfigPathsPlugin()];
 
     return config;
