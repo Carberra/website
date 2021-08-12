80316ac485abd9840dee8f05c8bc428b7e3a912f
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-08-12 10:35:09 -0400

    Add option to position spinner for certain pages

--- src/ui/Spinner.tsx
+++ src/ui/Spinner.tsx
@@ -4,16 +4,21 @@ import styles from '@styles/modules/Spinner.module.scss';
 
 export type SpinnerProps = {
   className?: string;
+  spinnerClassName?: string;
 };
 
 export const Spinner: React.FC<SpinnerProps> = (props) => {
   return (
     <div
-      className={`flex justify-center items-center ${styles.loadingSpinnerContainer} rounded-sm ${props.className}`}
+      className={`flex justify-center items-center ${
+        styles.loadingSpinnerContainer
+      } rounded-sm ${props.className || ''}`}
     >
-      <div
-        className={`w-9 h-9 bg-brand-gradient rounded-sm ${styles.loadingSpinner}`}
-      ></div>
+      <div className={`${props.spinnerClassName || ''}`}>
+        <div
+          className={`w-9 h-9 bg-brand-gradient rounded-sm ${styles.loadingSpinner}`}
+        ></div>
+      </div>
     </div>
   );
 };
