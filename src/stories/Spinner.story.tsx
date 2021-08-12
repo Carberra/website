1b4aa4340bc6f6debb00d3308f657b442b505958
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-08-12 09:30:30 -0400

    Remove mobile view (pointless)

--- src/stories/Spinner.story.tsx
+++ src/stories/Spinner.story.tsx
@@ -1,24 +1,9 @@
 import { Story } from '@storybook/react';
-import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { Spinner, SpinnerProps } from '@ui/Spinner';
 
 export default {
   title: 'Spinner',
   component: Spinner,
-  parameters: {
-    viewport: {
-      viewports: INITIAL_VIEWPORTS,
-      defaultViewport: 'reponsive',
-    },
-  },
 };
 
 export const Main: Story<SpinnerProps> = () => <Spinner />;
-
-export const Mobile: Story<SpinnerProps> = () => <Spinner />;
-
-Mobile.parameters = {
-  viewport: {
-    defaultViewport: 'iphonex',
-  },
-};
