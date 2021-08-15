08c98dd5590cade4b2d49aa711bfca9a4c18eb55
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-08-14 23:44:08 -0400

    Accept component prop

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -5,7 +5,7 @@ import ButtonBase from '@material-ui/core/ButtonBase';
 export type ButtonProps = DetailedHTMLProps<
   ButtonHTMLAttributes<HTMLButtonElement>,
   HTMLButtonElement
-> & { loading?: boolean };
+> & { loading?: boolean; component?: string };
 
 export const Button: React.FC<ButtonProps> = ({
   children,
@@ -14,6 +14,7 @@ export const Button: React.FC<ButtonProps> = ({
   id = '',
   className = '',
   type = 'button',
+  component = 'button',
 }) => {
   return (
     <ButtonBase
@@ -21,6 +22,7 @@ export const Button: React.FC<ButtonProps> = ({
       className={`bg-brand-gradient-hover text-white px-8 py-3 rounded-sm font-sans ${className}`}
       type={type}
       id={id}
+      component={component}
     >
       {children}
     </ButtonBase>
