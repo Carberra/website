51913fa26ca2d0434a7afdb706ea210eca9859e3
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-08-15 20:52:56 -0400

    Update mobile preview

--- src/stories/Navbar.story.tsx
+++ src/stories/Navbar.story.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
 import { Story } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { Navbar, NavbarProps } from '@ui/Navbar';
 
 export default {
   title: 'Navbar',
   component: Navbar,
+  parameters: {
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+      defaultViewport: 'reponsive',
+    },
+  },
 };
 
 export const Main: Story<NavbarProps> = () => <Navbar />;
 
-export const Mobile: Story<NavbarProps> = () => (
-  <div style={{ width: '508px' }}>
-    <Navbar />
-  </div>
-);
+export const Mobile: Story<NavbarProps> = () => <Navbar />;
+
+Mobile.parameters = {
+  viewport: {
+    defaultViewport: 'iphonex',
+  },
+};
