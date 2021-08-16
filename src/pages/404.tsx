c1e6120a8418b9527468e913db605cdd43c5c140
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-08-15 22:14:05 -0400

    Add header info

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React from 'react';
 
 import { Button } from '@ui/Button';
@@ -6,18 +7,24 @@ interface Props {}
 
 const PageNotFound: React.FC<Props> = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div>
-        <p className="text-brand-gradient text-7xl font-brand lowercase text-center mb-3">
-          404
-        </p>
-        <p className="text-white text-3xl font-sans text-center">
-          Page Not Found
-        </p>
-        <div className="flex justify-center">
-          <Button className="w-11/12 mt-4" href="/">
-            Return Home
-          </Button>
+    <div>
+      <Head>
+        <title>404 | Carberra Tutorials</title>
+        <meta name="description" content="The requested page doesn't exist" />
+      </Head>
+      <div className="flex justify-center items-center min-h-screen">
+        <div>
+          <p className="text-brand-gradient text-7xl font-brand lowercase text-center mb-3">
+            404
+          </p>
+          <p className="text-white text-3xl font-sans text-center">
+            Page Not Found
+          </p>
+          <div className="flex justify-center">
+            <Button className="w-11/12 mt-4" href="/">
+              Return Home
+            </Button>
+          </div>
         </div>
       </div>
     </div>
