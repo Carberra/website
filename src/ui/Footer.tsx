aa1ef05f41e02ca688f914f9a74d7320620e92ec
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-08-16 20:49:16 -0400

    Fix text compacting in footer

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -18,14 +18,14 @@ export const Footer: React.FC<FooterProps> = (props) => {
         <div className="flex md:block justify-center pr-5">
           <LogoWhite className="w-16 md:w-12" />
         </div>
-        <div className="md:justify-start w-full md:pl-5 md:pr-10 font-sans text-md">
+        <div className="md:justify-start md:pl-5 md:pr-10 font-sans text-md">
           <div className="flex justify-center md:justify-start pt-5 md:pt-0">
             <span className="pr-5 hover:underline">
               <Link href="/privacy-policy">Privacy Policy</Link>
             </span>
           </div>
         </div>
-        <div className="md:justify-end w-full md:pl-5 pt-2 md:pt-0">
+        <div className="md:justify-end flex-grow md:pl-5 pt-2 md:pt-0">
           <p className="font-sans text-md text-center md:text-right">
             Copyright (©) {new Date().getFullYear()} Carberra Tutorials
           </p>
