1d13d5b4d4b4081f0c3f6bea129e77e701b8d0eb
MrDogeBro <MrDogeBro@users.noreply.github.com>
2022-07-31 18:41:27 -0400

    Update to match new branding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./src/**/*.tsx', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   important: true,
   theme: {
-    fontFamily: {
-      sans: [
-        'Fira Sans',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        'Segoe UI',
-        'Helvetica',
-        'Arial',
-        'sans-serif',
-      ],
-      brand: [
-        'RaveParty Wide',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        'Segoe UI',
-        'Helvetica',
-        'Arial',
-        'sans-serif',
-      ],
-    },
     extend: {
+      fontFamily: {
+        sans: ['Anybody', ...defaultTheme.fontFamily.sans],
+        brand: ['Zen Dots'],
+      },
       colors: {
         'brand-black': 'var(--color-brand-black)',
         'brand-blue': 'var(--color-brand-blue)',
