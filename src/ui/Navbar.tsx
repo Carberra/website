972eb9b042a2172cb4f5dc592c160baeb608b721
MrDogeBro <MrDogeBro@users.noreply.github.com>
2022-07-31 21:40:56 -0400

    Add series page, update branding, and put drawer under navbar

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -4,7 +4,6 @@ import React, { useState } from 'react';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
 import { ThemeProvider } from '@mui/material/styles';
-import { makeStyles } from '@mui/styles';
 import Menu from '@mui/icons-material/Menu';
 
 import { muiTheme } from '@styles/theme';
@@ -18,16 +17,14 @@ interface PageData {
   location: string;
 }
 
-const styles = makeStyles((_: any) => ({
-  drawerPaper: {
-    backgroundColor: '#0c0c0c',
-  },
-}));
-
 export const Navbar: React.FC<NavbarProps> = ({ className = '' }) => {
   const [drawerState, setDrawer] = useState(false);
 
   const links: PageData[] = [
+    {
+      name: 'Series',
+      location: 'series',
+    },
     {
       name: 'About',
       location: 'about',
@@ -40,10 +37,10 @@ export const Navbar: React.FC<NavbarProps> = ({ className = '' }) => {
 
   return (
     <nav
-      className={`h-14 bg-brand-black flex justify-center items-center sticky w-full px-10 xs:px-20 ${className}`}
+      className={`h-14 bg-brand-black-mid flex justify-center items-center sticky z-20 w-full px-10 xs:px-20 ${className}`}
     >
       <Link href="/">
-        <span className="text-2xl font-brand text-brand-gradient lowercase cursor-pointer">
+        <span className="text-2xl font-brand text-brand-gradient uppercase cursor-pointer">
           Carberra
         </span>
       </Link>
@@ -54,7 +51,7 @@ export const Navbar: React.FC<NavbarProps> = ({ className = '' }) => {
         {links.map((link: PageData, i: number) => (
           <div key={i}>
             <Link href={link.location}>
-              <span className="font-sans text-xl text-white cursor-pointer ml-10">
+              <span className="font-sans font-extralight text-xl text-white cursor-pointer ml-10">
                 {link.name}
               </span>
             </Link>
@@ -80,16 +77,18 @@ export const Navbar: React.FC<NavbarProps> = ({ className = '' }) => {
             onClose={() => {
               setDrawer(false);
             }}
-            classes={{
-              //@ts-ignore
-              paper: styles().drawerPaper,
+            PaperProps={{
+              sx: {
+                background: 'var(--color-brand-black-light)',
+                zIndex: 10,
+              },
             }}
           >
-            <div className="w-40 mt-8">
+            <div className="w-40 mt-20">
               {links.map((link: PageData, i: number) => (
                 <div key={i} className="mr-10 flex justify-end items-center">
                   <Link href={link.location}>
-                    <span className="font-sans text-xl text-white cursor-pointer inline-block mb-2">
+                    <span className="font-sans font-extralight text-xl text-white cursor-pointer inline-block mb-2">
                       {link.name}
                     </span>
                   </Link>
