9eb19942101aee12e0985de76c2a53cfa4d74d9b
MrDogeBro <MrDogeBro@users.noreply.github.com>
2022-07-31 22:28:02 -0400

    Update font weights and logo for new branding

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react';
-
-import LogoWhite from '@public/brand/logo-white.svg';
 
 export type FooterProps = {
   className?: string;
@@ -16,9 +15,15 @@ export const Footer: React.FC<FooterProps> = (props) => {
     >
       <div className="block md:flex w-full md:items-center md:justify-start">
         <div className="flex md:block justify-center pr-5">
-          <LogoWhite className="w-16 md:w-12" />
+          <Image
+            src="/brand/logo-white.png"
+            // className="w-16 md:w-12"
+            width={40}
+            height={40}
+            className="m-0 p-0"
+          />
         </div>
-        <div className="md:justify-start md:pl-5 md:pr-10 font-sans text-md">
+        <div className="md:justify-start md:pl-5 md:pr-10 font-sans font-light text-md">
           <div className="flex justify-center md:justify-start pt-5 md:pt-0">
             <span className="pr-5 hover:underline">
               <Link href="/privacy-policy">Privacy Policy</Link>
@@ -26,7 +31,7 @@ export const Footer: React.FC<FooterProps> = (props) => {
           </div>
         </div>
         <div className="md:justify-end flex-grow md:pl-5 pt-2 md:pt-0">
-          <p className="font-sans text-md text-center md:text-right">
+          <p className="font-sans font-light text-md text-center md:text-right">
             Copyright (©) {new Date().getFullYear()} Carberra Tutorials
           </p>
         </div>
