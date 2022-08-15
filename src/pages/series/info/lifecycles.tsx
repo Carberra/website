a7800d64d2a770af3a1ef60546f4303f99945631
MrDogeBro <MrDogeBro@users.noreply.github.com>
2022-08-15 10:15:46 -0400

    Add page explaining series lifecycles

---
+++ src/pages/series/info/lifecycles.tsx
@@ -0,0 +1,67 @@
+import Head from 'next/head';
+import React from 'react';
+
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import remarkSlug from 'remark-slug';
+import rehypeRaw from 'rehype-raw';
+
+import { Navbar } from '@ui/Navbar';
+import { Footer } from '@ui/Footer';
+
+interface Props {}
+
+const SeriesLifecycles: React.FC<Props> = () => {
+  const lifecyclesContent = `
+Every tutorial series (episodic or otherwise) have a "lifecycle", defined here as the current running and support statuses. Every series has one of three statuses.
+
+## Active
+
+If a series is active, it is still receiving updates or has received an update recently. You will be able to get support for it in the comments or in the Discord server.
+
+## Dormant
+
+If a series is dormant, it is either on hiatus or has not received an update in the last four weeks. A dormant series may still receive updates, at which point it will become active again. You may be able to get support in the comments or in the Discord server depending on how long the series has been dormant.
+
+## Dead
+
+If a series is dead, that means it will no longer receive updates, even in the case of breaking changes. A series can die for a number of reasons:
+
+- It has been dormant for a long time and no updates are expected
+- Breaking changes mean the series no longer works
+- It has been superceded by a newer series
+
+You still may be able to receive support in the Discord server depending on the series' popularity and the circumstances surrounding its death, but you will likely not be able to get help in the comments.
+`;
+
+  return (
+    <div>
+      <Head>
+        <title>Series Lifecycles | Carberra Tutorials</title>
+        <meta name="description" content="Learn about Carberra Tutorials" />
+      </Head>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <div className="flex justify-center">
+          <h1 className="text-brand-gradient font-brand text-4xl mt-10 mb-5 mx-6 uppercase">
+            Series Lifecycles
+          </h1>
+        </div>
+
+        <div className="grow">
+          <div className="font-sans text-white text-md mt-4 mx-6 sm:mx-10 md:mx-20 lg:w-7/12 lg:mx-auto">
+            <ReactMarkdown
+              children={lifecyclesContent}
+              className="text-white font-sans unreset markdown grow"
+              remarkPlugins={[remarkGfm, remarkSlug]}
+              rehypePlugins={[rehypeRaw]}
+            />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default SeriesLifecycles;
