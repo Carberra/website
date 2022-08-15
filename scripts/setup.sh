595f8018ca17ead3dc8de63b458e00a87ece171f
MrDogeBro <MrDogeBro@users.noreply.github.com>
2022-08-15 11:41:22 -0400

    Remove scripts (replaced by github actions)

--- scripts/setup.sh
+++
@@ -1,6 +0,0 @@
-config_file=~/.bashrc
-
-echo "" >> $config_file
-echo "# automatically added for website scripts" >> $config_file
-echo "WEBSITE_DIRECTORY=$(pwd)" >> $config_file
-echo "export PATH=\$WEBSITE_DIRECTORY/scripts/bin:\$PATH" >> $config_file
