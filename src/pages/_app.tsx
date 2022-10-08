b9bb32c3e2cac5ce56fc5ca2dfce6efc7d78667b
MrDogeBro <MrDogeBro@users.noreply.github.com>
2022-10-08 14:49:40 -0400

    Remove meta tags from app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,10 @@
 import '@styles/globals.scss';
-import Head from 'next/head';
 // import Script from 'next/script';
 import type { AppProps } from 'next/app';
-
-import Meta from '@utils/meta';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
-      <Meta
-        title="Carberra"
-        description="The home of Carberra. Regardless of whether you're a budding programmer or an avid computer user, this is the place for you."
-      />
       {
         //@ts-ignore
         <Component {...pageProps} />
