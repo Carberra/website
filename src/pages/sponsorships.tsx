2a8c3b33929dfe2f934b106ddebb8ab62a0dc30f
Ethan Henderson <ethan.henderson.1998@gmail.com>
2024-08-19 12:16:25 +0100

    Update sponsorships page

--- src/pages/sponsorships.tsx
+++ src/pages/sponsorships.tsx
@@ -23,10 +23,13 @@ the main body of the video begins. These are done to camera, though
 footage can be overlayed.
 
 | Length (sec) | Price (GBP) | Price (USD) |
-| ------------ | ----------- | ----------- |
+| :----------: | :---------: | :---------: |
+| 0-5*         | 50          | 65          |
 | 30-45        | 200         | 250         |
 | 46-60        | 250         | 320         |
 | 61+          | _Enquire_   | _Enquire_   |
+
+_*These take the form of a quick mention._
 
 ## Placements
 
@@ -35,8 +38,12 @@ the product/service with no mentions, links, or calls to action. For
 these, see Dedicated/Videos.
 
 | Price (GBP) | Price (USD) |
-| ----------- | ----------- |
+| :---------: | :---------: |
 | 150         | 190         |
+
+## Other Integrations
+
+Please enquire for more information on other integration styles.
   `;
 
   const sponsorshipContentCol2 = `
@@ -44,28 +51,28 @@ these, see Dedicated/Videos.
 
 ## Videos
 
-Dedicated videos are only available for programming content. For other
-content, see Integrations or Dedicated/Shorts.
-
-Please note that due to UK advertising law, these must be labelled as
-advertisements in the thumbnail.
+These are only available for programming-related products and services.
+If you're looking to advertise something that is not programming
+related, please see **Integrations**.
 
 | Length (min) | Price (GBP) | Price (USD) |
-| ------------ | ----------- | ----------- |
-| 0-15         | 250         | 320         |
-| 16-20        | 300         | 380         |
-| 21+          | _Enquire_   | _Enquire_   |
+| :----------: | :---------: | :---------: |
+| 0-15         | 300         | 380         |
+| 16+          | _Enquire_   | _Enquire_   |
+
+_Please note that due to UK advertising law, these must be labelled as
+advertisements in the thumbnail._
 
 ## Shorts
 
-Please note that due to UK advertising law, the titles of these must
-start with #ad.
-
 | Length (sec) | Price (GBP) | Price (USD) |
-| ------------ | ----------- | ----------- |
+| :----------: | :---------: | :---------: |
 | 0-30         | 150         | 190         |
 | 31-45        | 200         | 250         |
 | 46-60        | 250         | 320         |
+
+_Please note that due to UK advertising law, the titles of these must
+start with #ad._
   `;
 
   return (
