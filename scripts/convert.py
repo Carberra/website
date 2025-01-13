a1b0a60672f98d752fc011efab5dd5e3b88577a8
Ethan Henderson <ethan.henderson.1998@gmail.com>
2025-01-13 15:57:42 +0000

    Convert USD to GBP as well

--- scripts/convert.py
+++ scripts/convert.py
@@ -4,5 +4,9 @@ from currency_converter import CurrencyConverter
 
 if __name__ == "__main__":
     c = CurrencyConverter()
-    converted = c.convert(amount := int(sys.argv[1]), "GBP", "USD")
-    print(f"{amount:.2f} GBP = {converted:.2f} USD")
+
+    to_usd = c.convert(amount := int(sys.argv[1]), "GBP", "USD")
+    print(f"{amount:.2f} GBP = {to_usd:.2f} USD")
+
+    to_gbp = c.convert(amount := int(sys.argv[1]), "USD", "GBP")
+    print(f"{amount:.2f} USD = {to_gbp:.2f} GBP")
